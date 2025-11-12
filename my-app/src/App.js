56af941f1b310633a39a6972c3951e75fd3fe236
ings <wk217008@dal.ca>
2025-11-12 15:19:44 -0400

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div className="container text-center">
+    <h1>Welcome to My Full-Stack App </h1>
+    <p>This is a React app styled with Bootstrap 5.</p>
+    </div>
+  );
+}
+export default App;
